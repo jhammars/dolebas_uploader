fa1d6c452f49d2b42fd08a6b4887b8edd95cd3df
Dolebas <johan.hammarstrm@gmail.com>
2017-06-28 12:21:28 +0200

    Add Wistia views field

---
+++ js/wistia-views-field.js
@@ -0,0 +1,21 @@
+(function ($, Drupal) {
+    Drupal.behaviors.wistiaUploadBehavior = {
+        attach: function (context, settings) {
+            window._wapiq = window._wapiq || [];
+            _wapiq.push(function(W) {
+                window.wistiaUploader = new W.Uploader({
+                    accessToken: settings.token,
+                    dropIn: "wistia_uploader",
+                    projectId: settings.project_id,
+                    beforeUpload: function() {
+                        wistiaUploader.setFileName(settings.nid);
+                    }
+                });
+                wistiaUploader.bind("uploadsuccess", function(file, media) {
+                    console.log("The upload succeeded. Here is the media object!", media);
+                    $(".wistia-video-id").val(media.id);
+                });
+            });
+        }
+    };
+})(jQuery, Drupal);
---
+++ src/Plugin/views/field/WistiaViewsField.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\dolebas_uploader\Plugin\views\field\WistiaViewsField.
+ */
+
+namespace Drupal\dolebas_uploader\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to display upload widget.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("dolebas_uploader_wistia_field")
+ */
+class WistiaViewsField extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+
+    $viewnid = strip_tags($this->view->field['nid']->original_value);
+    if ($viewnid) {
+      $nid = $viewnid;
+    }
+    else {
+      $nid = NULL;
+    }
+
+    $config = \Drupal::config('dolebas_uploader.settings');
+    $token = $config->get('wistia_token');
+    $project_id = $config->get('wistia_project_id');
+
+    $element['wistia']  = [
+      '#type' => 'inline_template',
+      '#theme' => 'wistia_views_field',
+      '#attached' => array(
+        'library' => array(
+          'dolebas_uploader/wistia-views-field'
+        ),
+        'drupalSettings' => array(
+          'nid' => $nid,
+          'token' => $token,
+          'project_id' => $project_id
+        )
+      )
+    ];
+    
+    return $element;
+  }
+}
